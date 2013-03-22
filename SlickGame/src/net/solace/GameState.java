231355b5479dd6991984de26e8bc6faa28f25a04
Corey James Carney <MobbyGFX96@hotmail.co.uk>
2013-03-22 12:03:52 +0000

    Added GameState class for Basic Game.

---
+++ SlickGame/src/net/solace/GameState.java
@@ -0,0 +1,33 @@
+package net.solace;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * @author 'MobbyGFX
+ */
+public class GameState extends BasicGameState {
+
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		
+	}
+
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+			throws SlickException {
+		
+	}
+
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		
+	}
+
+	public int getID() {
+		return 0;
+	}
+
+}
